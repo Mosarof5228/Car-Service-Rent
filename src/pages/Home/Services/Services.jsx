d1565c3a7fade0a016c545a0a8bba6e26504ed91
unknown <mosarofhossen5228@gmail.com>
2023-12-09 17:36:42 +0600

    fetch data form json file and show

---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+
+
+
+const Services = () => {
+    const [services,setServices]=useState([]);
+    console.log(services)
+   useEffect(()=>{
+    fetch('/public//Services.json')
+    .then(res=>res.json())
+    .then(data=>setServices(data))
+   },[])
+    return (
+        <div>
+          <div className="space-y-2 text-center py-5">
+            <p className="text-red-500">Services</p>
+            <h3 className="font-bold text-4xl">Our Services Area</h3>
+            <p>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+           {
+            services.map(service=><ServiceCard
+            key={service._id}
+            service={service}
+            ></ServiceCard>)
+           }
+          </div>
+        </div>
+    );
+};
+
+export default Services;
