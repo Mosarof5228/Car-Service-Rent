b0c9477730f24af1e07e7607368c86f241e3f7fe
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-12-23 00:40:55 +0600

    table data show

--- src/pages/Home/MyBookings/MyBookings.jsx
+++ src/pages/Home/MyBookings/MyBookings.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../../providers/AuthProviders";
+import BookingRow from "./BookingRow";
 
 
 const MyBookings = () => {
@@ -17,8 +18,37 @@ const MyBookings = () => {
     }, [])
 
     return (
-        <div>
-            <h2>Your Bookings Length: {bookings.length}</h2>
+        <div className="my-4">
+            <h2 className="text-center text-2xl py-4">Your Bookings Length: {bookings.length}</h2>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            bookings.map(booking => <BookingRow
+                                key={booking._id}
+                                booking={booking}
+                            ></BookingRow>)
+                        }
+                    </tbody>
+
+
+                </table>
+            </div>
         </div>
     );
 };
